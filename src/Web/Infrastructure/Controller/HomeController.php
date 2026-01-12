7db894d15ad9baa239cbf51594653fa6b17e2b4e
Alperen Uzun <alperenuzun@teknasyon.com>
2026-01-12 17:51:42 +0300

    feat: implement figma design page called welcome.

--- src/Web/Infrastructure/Controller/HomeController.php
+++ src/Web/Infrastructure/Controller/HomeController.php
@@ -1,34 +1,16 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Web\Infrastructure\Controller;
 
-use App\Api\Domain\Post\Repository\PostRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    public function __construct(
-        private readonly PostRepositoryInterface $postRepository
-    ) {
-    }
-
     #[Route('/', name: 'home', methods: ['GET'])]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        $publishedPosts = $this->postRepository->findPublished();
-
-        return $this->json([
-            'message' => 'Welcome to our blog!',
-            'published_posts' => array_map(fn($post) => [
-                'id' => $post->getId()->toString(),
-                'title' => $post->getTitle()->toString(),
-                'content' => substr($post->getContent()->toString(), 0, 200) . '...',
-                'publishedAt' => $post->getPublishedAt()?->format('Y-m-d H:i:s'),
-            ], $publishedPosts)
-        ]);
+        return $this->render('plus_ui/index.html.twig');
     }
 }
